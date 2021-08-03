2b8840b24334e89faf5d773c29b9356eb5e06b7c
Mani Peralta <mperalta@prominentmedia.com>
2021-08-03 12:12:39 +0100

    added project

---
+++ Views/Shared/Components/Contact/ContactController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Umbraco.Cms.Core.Cache;
+using Umbraco.Cms.Core.Logging;
+using Umbraco.Cms.Core.Routing;
+using Umbraco.Cms.Core.Services;
+using Umbraco.Cms.Core.Web;
+using Umbraco.Cms.Infrastructure.Persistence;
+using Umbraco.Cms.Web.Website.ActionResults;
+using Umbraco.Cms.Web.Website.Controllers;
+
+namespace B4.Views.Shared.Components.Contact
+{
+    public class ContactController : SurfaceController
+    {
+        public ContactController(IUmbracoContextAccessor umbracoContextAccessor, IUmbracoDatabaseFactory databaseFactory, ServiceContext services, AppCaches appCaches, IProfilingLogger profilingLogger, IPublishedUrlProvider publishedUrlProvider) : base(umbracoContextAccessor, databaseFactory, services, appCaches, profilingLogger, publishedUrlProvider) { }
+
+        [HttpPost]
+        public RedirectToUmbracoPageResult Submit(ContactForm model)
+        {
+            // Do some emails sending magic here.
+            ViewBag.ResponseMessage = "Thank you for your email";
+            return RedirectToCurrentUmbracoPage();
+        }
+    }
+}
